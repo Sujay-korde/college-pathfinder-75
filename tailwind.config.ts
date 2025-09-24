223a3596584c06ace9522c5fe3fc7421081d8b58
Sujay Korde <sujaykorde04@gmail.com>
2025-09-24 12:04:16 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
-        heading: ['Plus Jakarta Sans', 'system-ui', 'sans-serif'],
+        sans: ["General Sans", "Inter", "system-ui", "sans-serif"],
+        heading: ["General Sans", "Inter", "system-ui", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
